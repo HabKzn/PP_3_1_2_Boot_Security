6aa0b1f9feb12a810fcc81c400ee68ddf33e3896
Ayrat Sharipov <sharipov.airat@gmail.com>
2023-02-15 17:45:03 +0300

    temp

--- src/main/java/ru/kata/spring/boot_security/demo/service/UserServiceImpl.java
+++ src/main/java/ru/kata/spring/boot_security/demo/service/UserServiceImpl.java
@@ -4,23 +4,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.kata.spring.boot_security.demo.dao.UserRepository;
-import ru.kata.spring.boot_security.demo.model.Role;
 import ru.kata.spring.boot_security.demo.model.User;
 
 import java.util.List;
-import java.util.NoSuchElementException;
 import java.util.Optional;
 
 @Service
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
-    private final RoleService roleService;
 
     @Autowired
-    public UserServiceImpl(UserRepository userRepository, RoleService roleService) {
+    public UserServiceImpl(UserRepository userRepository) {
         this.userRepository = userRepository;
-        this.roleService = roleService;
     }
 
     @Override
--- src/main/resources/start_data.sql
+++ src/main/resources/start_data.sql
@@ -1,6 +1,24 @@
 
 INSERT INTO users (age, email, last_name, password, username)
 values (12, 'a','Adminov','a','Admin');
+
+INSERT INTO users (age, email, last_name, password, username)
+values (15, 'email2@email','Pupkin','password','Vasya');
+
+INSERT INTO users (age, email, last_name, password, username)
+values (12, 'email3@email','Petr','password2','Petrov');
+
+INSERT INTO users (age, email, last_name, password, username)
+values (17, 'email4@email','Ivanov','password3','Ivan');
+
+INSERT INTO users (age, email, last_name, password, username)
+values (20, 'b','Userov','b','User');
+
 INSERT INTO roles (name) values('ADMIN');
 INSERT INTO roles (name) values('USER');
+
 INSERT INTO users_roles(user_id, role_id) values (1, 1);
+INSERT INTO users_roles(user_id, role_id) values (2, 2);
+INSERT INTO users_roles(user_id, role_id) values (3, 2);
+INSERT INTO users_roles(user_id, role_id) values (4, 1);
+INSERT INTO users_roles(user_id, role_id) values (5, 2);
